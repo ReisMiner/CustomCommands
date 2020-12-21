37a350171d19d981a4b162c450ef97c0c892dd74
ReisMiner <r1miner@outlook.com>
2020-12-21 23:54:50 +0100

    fixed volume not working and breaking whole bot :(

--- src/main/java/xyz/reisminer/chtop/commands/music/Play.java
+++ src/main/java/xyz/reisminer/chtop/commands/music/Play.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.Event;
 import org.apache.commons.lang3.math.NumberUtils;
+import xyz.reisminer.chtop.music.GuildMusicManager;
 import xyz.reisminer.chtop.music.PlayerManager;
 
 public class Play {
@@ -15,10 +16,14 @@ public class Play {
         if (!self.getVoiceState().inVoiceChannel())
             Join.joiin(msg);
         if (splitMessage.length >= 2) {
-            PlayerManager.getInstance().loadAndPlay(channel, splitMessage[1]);
-            if (splitMessage.length > 2)
-                if (NumberUtils.isParsable(splitMessage[2]))
-                    PlayerManager.getInstance().getMusicManager(msg.getGuild()).audioPlayer.setVolume(Integer.parseInt(splitMessage[2]));
+            if (splitMessage.length > 2) {
+                if (NumberUtils.isParsable(splitMessage[2])) {
+                    PlayerManager.getInstance().loadAndPlay(channel, splitMessage[1]);
+                    final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(msg.getGuild());
+                    musicManager.scheduler.player.setVolume(Integer.parseInt(splitMessage[2]));
+                }
+            } else
+                PlayerManager.getInstance().loadAndPlay(channel, splitMessage[1]);
         } else
             channel.sendMessage("Please use the command like that: [PREFIX]play [YT-URL] [OPTIONAL: INT FOR VOLUME]").queue();
     }
