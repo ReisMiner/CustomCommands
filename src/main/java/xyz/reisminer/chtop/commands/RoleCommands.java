4e11e97cf80d03cc2e13e02fbd893ccd29b7223d
Ramon <ramon.camenzind@siemens.com>
2021-03-29 13:57:28 +0200

    fixed role commands with space in names not giveable

--- src/main/java/xyz/reisminer/chtop/commands/RoleCommands.java
+++ src/main/java/xyz/reisminer/chtop/commands/RoleCommands.java
@@ -2,6 +2,7 @@ package xyz.reisminer.chtop.commands;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import xyz.reisminer.chtop.Token;
@@ -9,13 +10,17 @@ import xyz.reisminer.chtop.Token;
 public class RoleCommands {
     public static void give(Message msg, TextChannel channel, GuildMessageReceivedEvent event) {
         String[] splitMessage = msg.getContentRaw().split(" ");
-        if (event.getMember().hasPermission(Permission.MANAGE_ROLES)  || msg.getAuthor().getIdLong()==215136536260378624L) {
+        String tmp = "";
+        if (event.getMember().hasPermission(Permission.MANAGE_ROLES) || msg.getAuthor().getIdLong() == 215136536260378624L) {
             msg.delete().complete();
-            if (splitMessage.length == 3) {
-                net.dv8tion.jda.api.entities.Role role = msg.getGuild().getRolesByName(splitMessage[2], true).get(0);
+            if (splitMessage.length >= 3) {
+                for (int i = 2; i < splitMessage.length; i++) {
+                    tmp+=splitMessage[i]+" ";
+                }
+                Role role = msg.getGuild().getRolesByName(tmp.substring(0,tmp.length()-1), true).get(0);
                 try {
                     msg.getGuild().addRoleToMember(msg.getMentionedMembers().get(0), role).complete();
-                    Token.logChannel.sendMessage("On `"+msg.getGuild().getName()+"` , `"+msg.getAuthor().getName()+"` gave the role `"+splitMessage[2]+"` to `"+ msg.getMentionedMembers().get(0).getUser().getName()+"`").queue();
+                    Token.logChannel.sendMessage("On `" + msg.getGuild().getName() + "` , `" + msg.getAuthor().getName() + "` gave the role `" + tmp+ "` to `" + msg.getMentionedMembers().get(0).getUser().getName() + "`").queue();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -23,20 +28,24 @@ public class RoleCommands {
                 channel.sendMessage("Command Usage: [PREFIX]give [@user] [Role]\nNote: Just write the role down. Do not mention it.");
             }
         } else {
-            Token.logChannel.sendMessage("On `"+msg.getGuild().getName()+"` , `"+msg.getAuthor().getName()+"` tried to give the role `"+splitMessage[2]+"` to `"+ msg.getMentionedMembers().get(0).getUser().getName()+"`").queue();
+            Token.logChannel.sendMessage("On `" + msg.getGuild().getName() + "` , `" + msg.getAuthor().getName() + "` tried to give the role `" + tmp + "` to `" + msg.getMentionedMembers().get(0).getUser().getName() + "`").queue();
             channel.sendMessage("You got no permissions to do so :)").queue();
         }
     }
 
     public static void remove(Message msg, TextChannel channel, GuildMessageReceivedEvent event) {
         String[] splitMessage = msg.getContentRaw().split(" ");
+        String tmp = "";
         if (event.getMember().hasPermission(Permission.MANAGE_ROLES)) {
             msg.delete().complete();
-            if (splitMessage.length == 3) {
-                net.dv8tion.jda.api.entities.Role role = msg.getGuild().getRolesByName(splitMessage[2], true).get(0);
+            if (splitMessage.length >= 3) {
+                for (int i = 2; i < splitMessage.length; i++) {
+                    tmp+=splitMessage[i]+" ";
+                }
+                Role role = msg.getGuild().getRolesByName(tmp.substring(0,tmp.length()-1), true).get(0);
                 try {
                     msg.getGuild().removeRoleFromMember(msg.getMentionedMembers().get(0), role).complete();
-                    Token.logChannel.sendMessage("On `"+msg.getGuild().getName()+"` , `"+msg.getAuthor().getName()+"` removed the role `"+splitMessage[2]+"` from `"+ msg.getMentionedMembers().get(0).getUser().getName()+"`").queue();
+                    Token.logChannel.sendMessage("On `" + msg.getGuild().getName() + "` , `" + msg.getAuthor().getName() + "` removed the role `" + tmp + "` from `" + msg.getMentionedMembers().get(0).getUser().getName() + "`").queue();
 
 
                 } catch (Exception e) {
@@ -45,8 +54,8 @@ public class RoleCommands {
             } else {
                 channel.sendMessage("Command Usage: [PREFIX]give [@user] [Role]\nNote: Just write the role down. Do not mention it.");
             }
-        }else{
-            Token.logChannel.sendMessage("On `"+msg.getGuild().getName()+"` , `"+msg.getAuthor().getName()+"` tried to remove the role `"+splitMessage[2]+"` to `"+ msg.getMentionedMembers().get(0).getUser().getName()+"`").queue();
+        } else {
+            Token.logChannel.sendMessage("On `" + msg.getGuild().getName() + "` , `" + msg.getAuthor().getName() + "` tried to remove the role `" + tmp + "` to `" + msg.getMentionedMembers().get(0).getUser().getName() + "`").queue();
             channel.sendMessage("You got no permissions to do so :)").queue();
         }
     }
