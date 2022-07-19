2126f7e4fe8f59082a45e29288722161c1eac05d
ReisMiner <r1miner@outlook.com>
2022-07-19 22:45:15 +0200

    lookup by id

--- src/main/java/xyz/reisminer/chtop/commands/gamble/getPeterZ.java
+++ src/main/java/xyz/reisminer/chtop/commands/gamble/getPeterZ.java
@@ -3,16 +3,40 @@ package xyz.reisminer.chtop.commands.gamble;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
+import org.apache.commons.lang3.StringUtils;
 
 import static xyz.reisminer.chtop.commands.gamble.gambleDB.*;
 
 public class getPeterZ {
 
     public static void getWallet(Message msg, MessageChannel channel) {
-
+        String[] splitMessage = msg.getContentRaw().split(" ");
         User author = msg.getAuthor();
-        if (!msg.getMentionedMembers().isEmpty()) {
-            User checkFrom = msg.getMentionedMembers().get(0).getUser();
+        if (splitMessage.length == 2) {
+            User checkFrom = null;
+            boolean error = false;
+            if (!msg.getMentionedMembers().isEmpty()) {
+                checkFrom = msg.getMentionedMembers().get(0).getUser();
+            } else if (StringUtils.isNumeric(splitMessage[1])) {
+                try {
+                    checkFrom = msg.getJDA().getUserById(Long.parseLong(splitMessage[1]));
+                } catch (Exception ignore) {
+                    error = true;
+                }
+            } else {
+                checkFrom = getUserByName(splitMessage[1], msg);
+                try {
+                    checkFrom.getIdLong();
+                } catch (NullPointerException e) {
+                    error = true;
+                }
+            }
+
+            if(error){
+                channel.sendMessage("Error looking up balance!").queue();
+                return;
+            }
+
             if (!userExists(checkFrom)) {
                 addNewUser(checkFrom);
             }
