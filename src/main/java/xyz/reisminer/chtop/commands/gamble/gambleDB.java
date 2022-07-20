b96de23b9bac43516a8af3cc63019603b23b0d75
ReisMiner <r1miner@outlook.com>
2022-07-20 19:44:22 +0200

    updated leaderboard command

--- src/main/java/xyz/reisminer/chtop/commands/gamble/gambleDB.java
+++ src/main/java/xyz/reisminer/chtop/commands/gamble/gambleDB.java
@@ -5,76 +5,82 @@ import net.dv8tion.jda.api.entities.User;
 import xyz.reisminer.chtop.Token;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class gambleDB {
     public static void addNewUser(User user) {
         try (Connection connection = DriverManager.getConnection(Token.DBurl, Token.DBusername, Token.DBpassword)) {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query
-            String s =String.format("INSERT INTO currencies (IDLong, `username`, amount) values ( '%d', '%s', '%d')",user.getIdLong(),user.getName(),50);
+            String s = String.format("INSERT INTO currencies (IDLong, `username`, amount) values ( '%d', '%s', '%d')", user.getIdLong(), user.getName(), 50);
             statement.executeUpdate(s);
             System.out.println("Created new user in DB: " + user.getName());
         } catch (SQLException e) {
-            System.err.println("Add User: DB Error:\n"+e);
+            System.err.println("Add User: DB Error:\n" + e);
         }
     }
+
     public static boolean userExists(User user) {
         try (Connection connection = DriverManager.getConnection(Token.DBurl, Token.DBusername, Token.DBpassword)) {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query
-            ResultSet resultSet = statement.executeQuery("select * from currencies where IDLong like "+user.getIdLong());
+            ResultSet resultSet = statement.executeQuery("select * from currencies where IDLong like " + user.getIdLong());
             return resultSet.next();
         } catch (SQLException e) {
-            System.err.println("Check user: DB Error:\n"+e);
+            System.err.println("Check user: DB Error:\n" + e);
             return true;
         }
     }
+
     public static int getCurrencyOfUser(User user) {
-        int balance=-1337;
+        int balance = -1337;
         try (Connection connection = DriverManager.getConnection(Token.DBurl, Token.DBusername, Token.DBpassword)) {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query
-            ResultSet resultSet = statement.executeQuery("select * from currencies where IDLong like "+user.getIdLong()+"");
+            ResultSet resultSet = statement.executeQuery("select * from currencies where IDLong like " + user.getIdLong() + "");
             if (resultSet.next()) {
-                balance=resultSet.getInt(3);
+                balance = resultSet.getInt(3);
             }
         } catch (SQLException e) {
-            System.err.println("DB Error:\n"+e);
+            System.err.println("DB Error:\n" + e);
 
         }
         return balance;
     }
+
     public static void changeBalance(User user, int amount) {
         try (Connection connection = DriverManager.getConnection(Token.DBurl, Token.DBusername, Token.DBpassword)) {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query update currencies set amount=amount+1 where username like 'ReisMiner'
-            String s =String.format("UPDATE currencies set amount=amount+%d where `IDLong` like '%d'",amount,user.getIdLong());
+            String s = String.format("UPDATE currencies set amount=amount+%d where `IDLong` like '%d'", amount, user.getIdLong());
             statement.executeUpdate(s);
         } catch (SQLException e) {
-            System.err.println("Chanbe balance: DB Error:\n"+e);
+            System.err.println("Chanbe balance: DB Error:\n" + e);
         }
     }
-    public static String getLeaderBoard(int userCount) {
-        StringBuilder retun = new StringBuilder();
-        retun.append("```Top ").append(userCount).append(" :\n");
+
+    public static Map<Long, Double> getLeaderBoard(int userCount) {
+
+        Map<Long, Double> res = new HashMap<>();
+
         try (Connection connection = DriverManager.getConnection(Token.DBurl, Token.DBusername, Token.DBpassword)) {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query
             ResultSet resultSet = statement
-                    .executeQuery("select * from currencies order by amount desc limit "+userCount);
+                    .executeQuery("select * from currencies order by amount desc limit " + userCount);
             int i = 1;
             while (resultSet.next()) {
-                String name = resultSet.getString("username");
+                Long idLong = resultSet.getLong("IDLong");
                 Double amount = resultSet.getDouble("amount");
-                retun.append(" ").append(i).append(". ").append(name).append(" ").append(amount).append("\n");
+                res.put(idLong, amount);
                 i++;
             }
-            retun.append("```");
         } catch (SQLException e) {
-            retun.append("`Couldn't connect to the Database.`");
-            System.err.println("DB Error:\n"+e);
+            System.err.println("DB Error:\n" + e);
+            return null;
         }
-        return retun.toString();
+        return res;
     }
 
     public static User getUserByName(String name, Message msg) {
@@ -83,12 +89,12 @@ public class gambleDB {
             Statement statement = connection.createStatement();
             // Result set get the result of the SQL query
             ResultSet resultSet = statement
-                    .executeQuery("select * from currencies where username like '"+name+"'");
+                    .executeQuery("select * from currencies where username like '" + name + "'");
             if (resultSet.next()) {
                 retunIDLong = resultSet.getLong("IDLong");
-                            }
+            }
         } catch (SQLException e) {
-            System.err.println("DB Error:\n"+e);
+            System.err.println("DB Error:\n" + e);
         }
 
         return msg.getJDA().getUserById(retunIDLong);
--- src/main/java/xyz/reisminer/chtop/commands/gamble/leaderboard.java
+++ src/main/java/xyz/reisminer/chtop/commands/gamble/leaderboard.java
@@ -1,18 +1,43 @@
 package xyz.reisminer.chtop.commands.gamble;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class leaderboard {
     public static void showTopTen(Message msg, MessageChannel channel) {
         String[] splitMessage = msg.getContentRaw().split(" ");
+
+        Map<Long, Double> result = new HashMap<>();
         if (splitMessage.length == 2) {
             int count = Integer.parseInt(splitMessage[1]);
-            channel.sendMessage(gambleDB.getLeaderBoard(count)).queue();
+            result = gambleDB.getLeaderBoard(count);
         } else if(splitMessage.length==1) {
-            channel.sendMessage(gambleDB.getLeaderBoard(10)).queue();
+            result = gambleDB.getLeaderBoard(10);
         }else{
             channel.sendMessage("CMD Usage: [PREFIX]leaderboard <optional: how many users u wanna see>").queue();
         }
+
+        if(result == null){
+            channel.sendMessage("Database Error!").queue();
+            return;
+        }
+
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("Currency Leaderboard");
+
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(1);
+        result.forEach((k,v) ->{
+            sb.append(String.format("**%d.** <@%d> - %d peterZ\n",count.get(),k,v));
+            count.getAndIncrement();
+        });
+        eb.setDescription(sb.toString());
+
+        channel.sendMessageEmbeds(eb.build()).queue();
     }
 }
